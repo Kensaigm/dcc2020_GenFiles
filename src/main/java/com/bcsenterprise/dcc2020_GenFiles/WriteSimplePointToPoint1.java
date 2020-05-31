61813e0ab33c7070aa8ab36bef36c3dbfbe52615
Kensaigm <Kensaigm@users.noreply.github.com>
2020-05-31 08:18:15 -0800

    Update WriteSimplePointToPoint1.java
    Added imports

--- src/main/java/com/bcsenterprise/dcc2020_GenFiles/WriteSimplePointToPoint1.java
+++ src/main/java/com/bcsenterprise/dcc2020_GenFiles/WriteSimplePointToPoint1.java
@@ -1,14 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.bcsenterprise.dcc2020_GenFiles;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 /**
  *
  * @author BCS Actual
  */
 public class WriteSimplePointToPoint1 {
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args)
+    {
     
+    }
 }
