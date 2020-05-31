01d5628f419a66a0a189bdbd51e7c5ea2acfaca5
Kensaigm <Kensaigm@users.noreply.github.com>
2020-05-30 17:40:40 -0700

    Create ReadJSON.java
    Added ReadJSON

---
+++ src/main/java/com/bcsenterprise/dcc2020_GenFiles/ReadJSON.java
@@ -0,0 +1,56 @@
+
+package com.bcsenterprise.dcc2020_GenFiles;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+/**
+ *
+ * @author scott
+ */
+public class ReadJSON {
+    
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args){
+        //JSON parser object to parse read file
+        JSONParser jsonParser = new JSONParser();
+        
+        try (FileReader reader = new FileReader("index.json")){
+            // Read the JSON file
+            Object obj = jsonParser.parse(reader);
+            
+            JSONArray fileList = (JSONArray) obj;
+            
+            // Iterate over file array
+            fileList.forEach(jFile -> parseUserObject((JSONObject) jFile));
+        
+        } catch (FileNotFoundException fnfe){
+            fnfe.printStackTrace();
+        } catch (IOException ioe){
+            ioe.printStackTrace();
+        } catch (ParseException pe) {
+            pe.printStackTrace();
+        }
+    }
+    
+    private static void parseUserObject(JSONObject jFile){
+        
+        // Get File name
+        String fName = (String)jFile.get("name").toString();
+        System.out.println("File Name:" + fName);
+        
+        float key = (Float)jFile.get("key");
+        System.out.println("Key:" + key);
+        
+        boolean enabled = (boolean)jFile.get("enabled");
+        System.out.println("Enabled ==" + enabled);
+        
+    }
+    
+}
