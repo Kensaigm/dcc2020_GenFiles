5f5d7fe354edfd2e947ad10a3f0d60db3e83d7c8
Kensaigm <Kensaigm@users.noreply.github.com>
2020-05-31 08:15:21 -0800

    Create WriteSimplePointToPoint1.java
    Added new file to setup testing.

---
+++ src/main/java/com/bcsenterprise/dcc2020_GenFiles/WriteSimplePointToPoint1.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bcsenterprise.dcc2020_GenFiles;
+
+/**
+ *
+ * @author BCS Actual
+ */
+public class WriteSimplePointToPoint1 {
+    
+}
