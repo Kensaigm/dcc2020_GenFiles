a467af8526194c72d539841fb45f52598c405092
Kensaigm <Kensaigm@users.noreply.github.com>
2020-05-31 08:20:15 -0800

    Update WriteSimplePointToPoint1.java
    Added some test data

--- src/main/java/com/bcsenterprise/dcc2020_GenFiles/WriteSimplePointToPoint1.java
+++ src/main/java/com/bcsenterprise/dcc2020_GenFiles/WriteSimplePointToPoint1.java
@@ -14,6 +14,9 @@ public class WriteSimplePointToPoint1 {
     @SuppressWarnings("unchecked")
     public static void main(String[] args)
     {
-    
+        JSONObject jo = new JSONObject();
+        jo.put("manager", "mouse");
+        jo.put("X", 100);
+        jo.put("Y", 100);
     }
 }
