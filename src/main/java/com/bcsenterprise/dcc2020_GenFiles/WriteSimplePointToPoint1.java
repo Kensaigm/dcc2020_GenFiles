0344957ed26459a160477170b56f96ddfb563949
Kensaigm <Kensaigm@users.noreply.github.com>
2020-05-31 08:57:23 -0800

    Update WriteSimplePointToPoint1.java
    Changed the data format again.

--- src/main/java/com/bcsenterprise/dcc2020_GenFiles/WriteSimplePointToPoint1.java
+++ src/main/java/com/bcsenterprise/dcc2020_GenFiles/WriteSimplePointToPoint1.java
@@ -16,19 +16,21 @@ public class WriteSimplePointToPoint1 {
     @SuppressWarnings("unchecked")
     public static void main(String[] args)
     {
-        JSONObject jo = new JSONObject();
-        jo.put("manager", "mouse");
-        
         JSONArray ja = new JSONArray();
-        ja.add("X");
-        ja.add(100);
-        ja.add("Y");
-        ja.add(100);
         
-        jo.put("Position", ja);
+        JSONObject jo1 = new JSONObject();
+        jo1.put("manager", "mouse");
         
-        jo.put("manager", "mouse");
+        JSONArray ja1 = new JSONArray();
+        ja1.add("X");
+        ja1.add(100);
+        ja1.add("Y");
+        ja1.add(100);
         
+        jo1.put("Position", ja1);
+        
+        JSONObject jo2 = new JSONObject();
+        jo2.put("manager", "mouse");
         
         JSONArray ja2 = new JSONArray();
         ja2.add("X");
@@ -36,10 +38,15 @@ public class WriteSimplePointToPoint1 {
         ja2.add("Y");
         ja2.add(200);
         
-        jo.put("Position", ja2);
+        jo2.put("Position", ja2);
+        
+        ja.add(jo1);
+        ja.add(ja1);
+        ja.add(jo2);
+        ja.add(ja2);
         
         try (FileWriter fw = new FileWriter("SimplePointToPoint.json")){
-            fw.write(jo.toJSONString());
+            fw.write(ja.toJSONString());
         } catch (IOException ex) {
             Logger.getLogger(WriteSimplePointToPoint1.class.getName()).log(Level.SEVERE, null, ex);
         } 
