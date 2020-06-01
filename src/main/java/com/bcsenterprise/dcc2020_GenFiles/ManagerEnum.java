7dcdfee4603d185bcca90909bee5cb0621526c02
Kensaigm <Kensaigm@users.noreply.github.com>
2020-06-01 00:56:37 -0700

    Update ManagerEnum.java
    Added MSE/KBRD as valid manager types.

--- src/main/java/com/bcsenterprise/dcc2020_GenFiles/ManagerEnum.java
+++ src/main/java/com/bcsenterprise/dcc2020_GenFiles/ManagerEnum.java
@@ -6,5 +6,16 @@ package com.bcsenterprise.dcc2020_GenFiles;
  * @author scott
  */
 public enum ManagerEnum {
+    MSE("Mouse"),
+    KBRD("Keyboard");
     
+    private String manager;
+    
+    ManagerEnum(String chkManager){
+        this.manager = chkManager;
+    }
+    
+    public String getManager(){
+        return manager;
+    }
 }
